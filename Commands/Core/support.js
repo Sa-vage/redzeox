a650db8a32cdeb4f36e25e78171cc2a5f9fc4c4c
ʀᴇᴅᴢᴇᴏꭗ <105351945+REDZEOX@users.noreply.github.com>
2023-03-05 19:52:30 +0530

    🕯️✨ʀᴇᴅᴢᴇᴏꭗ✨🕯️

--- Commands/Core/support.js
+++ Commands/Core/support.js
@@ -8,7 +8,7 @@ module.exports = {
     start: async(Miku, m,{pushName}) => {
         m.reply(`🍃Check your DM *${pushName}* Senpai !\n\n✨I have sent you support group link personally.`)
         let botpic = botImage1
-        let txt = `      🧣 *Support Group* 🧣\n\n*${botName}* 🐰is an open source project, and we are always happy to help you.\n\n*Link:* ${suppL}\n\n*Note:* 🐷Please don't spam in the group, and don't message *Admins directly* without permission. Ask for help in *Group*.\n\n*🍑Thanks for using Rias Gremory.*`
+        let txt = `      📌 *Support Group* 📌\n\n*${botName}* 🐰is an open source project, and we are always happy to help you.\n\n*Link:* ${suppL}\n\n*Note:*Please don't spam in the group, and don't message *Admins directly* without permission. Ask for help in *Group*.\n\n*Thanks for using Rias Gremory.*`
         await Miku.sendMessage(m.sender,{image:{url:botpic}, caption:txt},{quoted:m})
     }
 }
